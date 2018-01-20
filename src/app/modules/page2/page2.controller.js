c16221a7947f2039520f0a2bb8c5cecc693007ea
carlos <carlosperezarandojo@gmail.com>
2018-01-20 18:34:50 +0100

    Añadido JS a la pagina de inicio. Añadido controlador al route y html formulario

--- src/app/modules/page2/page2.controller.js
+++ src/app/modules/page2/page2.controller.js
@@ -1,66 +1,65 @@
+(function() {
+  'use strict';
 
   angular
-  .module('angularjs')
-  .controller('Page2Controller', ['$scope', function($scope) {
-   // .controller('Page2Controller', Page2Controller);
+    .module('angularjs')
+    .controller('Page2Controller', Page2Controller);
 
-   	console.log('[Page2Controller] Inicio controlador:  Page2Controller');
+  /** @ngInject */
+  function Page2Controller() {
+   
+    console.log('[Page2Controller] Inicio controlador:  Page2Controller');
 
-   	//controla que se muestre un texto en rojo si alguno de los campos no ha sido rellenado.
-   	$scope.show=false;
+	//controla que se muestre un texto en rojo si alguno de los campos no ha sido rellenado.
+   	this.show=false;
+	this.name = '';
+	this.username = '';
+	this.email = '';
+	this.phone = '';
+   
 
-   	$scope.addUser = function() {
+
+	this.addUser = function (){
        
    		console.log('[Page2Controller] Inicio funcion addUser ');
 
-   		if(validateFields()){
+   		if(validateFields(this)){
 
    			console.log('Todos los campos han sido informados.');
-   			$scope.show=false;
 
+   			//ocultamos el texto de error
+   			this.show=false;
 
+   			//creamos json con los datos introducidos en el formulario.
+			var userJson = JSON.stringify(
+	   			{
+			      name: this.name,
+			      username: this.username,
+			      email: this.email,
+			      phone: this.phone,
+			      userId: 1
+			    }
+			)
 
+   			console.log('userJson: ', userJson);
 
-
+   			//TODO llamamos al servicio
 
 
 
 
    		}else{
    			console.log('Alguno o ninguno de los campos ha sido informado.');
-   			$scope.show=true;
+   			this.show=true;
    		}
-
-
-   		var userJson = JSON.stringify(
-   			{
-		      name: $scope.name,
-		      username: $scope.username,
-		      email: $scope.email,
-		      phone: $scope.phone,
-		      userId: 0
-		    }
-		)
-
-   		console.log('userJson: ', userJson);
-
-
-
-
 
    		console.log('[Page2Controller] Fin funcion addUser ');
     };
 
-	console.log('[Page2Controller] Fin controlador:  Page2Controller');
 
 
 
-
-
-
-
-
-	/**********
+     /**********
 	
 	funciones privadas
 
@@ -70,16 +69,27 @@
 	* Validamos que todos los campos esten informados antes de realizar la petición.
 	* @return boolean
 	**/
-   	function validateFields(){
+   	function validateFields(data){
+
+   		console.log('Validando campos');
 
    		var valid = true;
 
-   		if(undefined==$scope.name || undefined===$scope.username || undefined==$scope.email || undefined===$scope.phone){
+   		if(undefined===data.name || undefined===data.username || undefined==data.email || undefined===data.phone || 
+   			""===data.name || ""===data.username || ""==data.email || ""===data.phone){
    			valid = false
    		}
+
+	console.log('Campos validados: ', valid);
 
    		return valid;
    	};
 
+ 	
+ 	console.log('[Page2Controller] Fin controlador:  Page2Controller');
 
-}]);
+
+  }//end controller.
+ 
+
+})();
