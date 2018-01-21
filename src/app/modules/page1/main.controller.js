c4f84632fdfc8f2a9369ca6f0f7be66906e67897
David Sanchez <david85trabajo@gmail.com>
2018-01-21 09:33:49 +0100

    prueba de llamada al servicio de listado

--- src/app/modules/page1/main.controller.js
+++ src/app/modules/page1/main.controller.js
@@ -9,5 +9,29 @@
   function MainController() {
     var vm = this;
 
+	this.getPosts = function(){
+		
+		fetch('https://jsonplaceholder.typicode.com/users')
+		  .then(status)
+		  .then(function(response){
+			  return response.json()
+		  })
+		  .then(function(data) {
+			console.log(data);
+		  })
+		  .catch(function(error) {
+			console.log('Fetch Error :-S', error);
+		  });
+		
+	}
   }
+  
+  function status(response) {
+	  if (response.status >= 200 && response.status < 300) {
+		return Promise.resolve(response)
+	  } else {
+		return Promise.reject(new Error(response.statusText))
+	  }
+	}
+
 })();
