7abea412ca64d96270844605a315c91e86b4a8be
carlos <carlosperezarandojo@gmail.com>
2018-01-21 18:28:45 +0100

    Modificación modelo de datos de usuario

--- src/app/modules/users/addUser.controller.js
+++ src/app/modules/users/addUser.controller.js
@@ -18,6 +18,8 @@
   	//controla que se muestre un texto en rojo si alguno de los campos no ha sido informado.
     this.show=false;
 
+    //modelo donde almacenamos los datos del formulario.
+    this.user = {};
 
 
 
@@ -28,7 +30,7 @@
        
    		console.log('[AddUserController] Inicio funcion addUser ');
 
-   		if(validateFields(this)){
+   		if(validateFields(this.user)){
 
    			console.log('Todos los campos han sido informados.');
 
@@ -36,23 +38,15 @@
    			this.show=false;
 
    			//creamos json con los datos introducidos en el formulario.
-        var userJson = JSON.stringify(
-	   			{
-			      name: this.name,
-			      username: this.username,
-			      email: this.email,
-			      phone: this.phone
-			    }
-			  );
+        var userJson = getFieldData(this.user);
 
         //realizamos la llamada al servicio.
         usersService.addUser(userJson).then(function(response){
          
           console.log('response', response);
 
-        });
-       
-          
+        });      
+        
         
    		}else{
    			console.log('Alguno o ninguno de los campos ha sido informado.');
@@ -62,6 +56,10 @@
    		console.log('[AddUserController] Fin funcion addUser ');
 
     };
+
+
+
+
 
  	console.log('[AddUserController] Fin controlador:  AddUserController');
 
@@ -97,4 +95,21 @@
       console.log('Campos validados. Son correctos? ', valid);
 
    		return valid;
+  };
+
+  /**
+  * Función que obtiene los datos del usuario informados en el formulario
+  * @param modelo del usuario.
+  **/
+  function getFieldData(userModel){
+
+
+    return JSON.stringify(
+          {
+            name: userModel.name,
+            username: userModel.username,
+            email: userModel.email,
+            phone: userModel.phone
+          }
+        );
   };
